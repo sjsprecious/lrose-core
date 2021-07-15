c93ff6a436cbfc2c2d3283ec6af47bff6138506b
Jian Sun <sjsprecious@gmail.com>
2021-07-15 16:00:44 -0600

    update linker flag

--- build/cmake/createCMakeLists.py
+++ build/cmake/createCMakeLists.py
@@ -644,7 +644,7 @@ def writeCMakeListsTop(dir):
     fo.write('set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)\n')
     fo.write('\n')
     fo.write('if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")\n')
-    fo.write('  set(CMAKE_EXE_LINKER_FLAGS "-Wl,--enable-new-dtags")\n')
+    fo.write('  set(CMAKE_EXE_LINKER_FLAGS "-Wl,--enable-new-dtags -L'+options.prefix+'/lib")\n')
     fo.write('endif()\n')
     fo.write('\n')
 
